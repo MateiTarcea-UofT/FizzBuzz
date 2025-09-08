9decd0bf7994d684168a0dba1bc66f6db79e9c77
MateiTarcea-UofT <matei.tarcea@mail.utoronto.ca>
2025-09-08 10:46:54 -0400

    Restored test

--- test/lab1/MultiplesDefaultTest.java
+++ test/lab1/MultiplesDefaultTest.java
@@ -10,7 +10,7 @@ class MultiplesDefaultTest {
     @Test
     public void testDefault() {
         assertEquals(466,
-                Multiples.multiples(1000,3,5),
+                Multiples.multiples(),
                 "Multiples.multiples() is incorrect for default values");
     }
 
