00bfd3a36ec2279b4fccf1693b8c704ee3824759
MateiTarcea-UofT <matei.tarcea@mail.utoronto.ca>
2025-09-08 10:34:34 -0400

    Added Multiples test to default root test folder.

---
+++ test/lab1/MultiplesDefaultTest.java
@@ -0,0 +1,17 @@
+package lab1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+  Test for overloading multiples.
+ */
+class MultiplesDefaultTest {
+    @Test
+    public void testDefault() {
+        assertEquals(466,
+                Multiples.multiples(1000,3,5),
+                "Multiples.multiples() is incorrect for default values");
+    }
+
+}
