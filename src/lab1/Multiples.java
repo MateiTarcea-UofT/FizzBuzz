6abc80a12811a909f687e4d3b678cc18209d0e6e
MateiTarcea-UofT <matei.tarcea@mail.utoronto.ca>
2025-09-08 10:46:34 -0400

    Added overloaded method and fixed paramaterized method logic

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,14 +2,25 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        int count = multiples(1000, 3,5);
+        int count = multiples(1000, 4,4);
         System.out.println(count);
+    }
+
+    public static int multiples(){
+        int count = 0;
+        for (int j = 1; j < 1000; j++) {
+            if (j % 3 == 1 || j % 5 == 1){
+                count++;
+            }
+        }
+        return count;
     }
 
     public static int multiples(int n, int a, int b) {
         int count = 0;
         for (int j = 1; j < n; j++) {
-            if (j % a == 1 || j % b == 1){
+            if (j % a == 0 || j % b == 0){
+                System.out.println(j);
                 count++;
             }
         }
